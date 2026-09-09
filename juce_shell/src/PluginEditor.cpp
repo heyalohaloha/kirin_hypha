5e04fcca79a1f70c76c7d23c256ff16568024fee
Daisuke Nishio <hey.alohaloha@gmail.com>
2026-09-09 14:23:21 +0900

    [B-771] Extract editor lifecycle boundary

--- juce_shell/src/PluginEditor.cpp
+++ juce_shell/src/PluginEditor.cpp
@@ -283,24 +283,6 @@ KirinHyphaEditor::KirinHyphaEditor (KirinHyphaProcessorBase& p)
    #endif
 }
 
-KirinHyphaEditor::~KirinHyphaEditor()
-{
-    stopTimer();
-    commitEditorSizeStateIfSettled (true);
-    tooltip.setLookAndFeel (nullptr);
-    if (isPost)
-    {
-        processorRef.endReferenceBlind();
-        processorRef.setSpectrumVisible (false);
-        processorRef.setPerceptualVisible (false);
-        processorRef.setAbsoluteVisible (false);
-       #if ! KIRIN_HYPHA_PRE_DISPLAY
-        processorRef.setAttackEnabled (false);
-       #endif
-    }
-}
-
-
 juce::String KirinHyphaEditor::instanceId8() const
 {
     return processorRef.instanceId().substring (0, 8);
@@ -506,45 +488,4 @@ void KirinHyphaEditor::updateFeedback (
         feedbackLabel.setText (text, juce::dontSendNotification);
         feedbackLabel.setColour (juce::Label::textColourId, colour);
     }
-}
-
-
-void KirinHyphaEditor::timerCallback()
-{
-    commitEditorSizeStateIfSettled (false);
-#if KIRIN_HYPHA_GUIDE_TRANSPORT
-    const auto attachment = processorRef.takeCaptureWorkAttachmentResult();
-    if (attachment.state == hypha::capture::WorkAttachmentResultState::attached)
-        showToast ("Capture attached to Work");
-    else if (attachment.state == hypha::capture::WorkAttachmentResultState::rejected)
-    {
-        if (attachment.code == "work_binding_changed")
-            showToast ("Work connection changed; Capture was not attached");
-        else if (attachment.code == "work_not_found")
-            showToast ("Connected Work is no longer available");
-        else if (attachment.code == "work_record_invalid"
-                 || attachment.code == "work_write_failed"
-                 || attachment.code == "destination_write_failed")
-            showToast ("Work could not be updated");
-        else if (attachment.code == "request_write_failed"
-                 || attachment.code == "artifact_invalid"
-                 || attachment.code == "request_invalid")
-            showToast ("Capture could not be attached");
-        else
-            showToast ("Kirin OS could not attach this Capture");
-    }
-#endif
-    if (isPost) updatePost();
-    else        updatePre();
-    refreshObservatory();
-}
-
-void KirinHyphaEditor::commitEditorSizeStateIfSettled (bool force)
-{
-    constexpr double settleSeconds = 0.2;
-    if (! editorSizeStateDirty
-        || (! force && nowSecs() - editorSizeLastChangedAt < settleSeconds))
-        return;
-    editorSizeStateDirty = false;
-    processorRef.notifyObservatoryEditorSizeChanged();
 }
