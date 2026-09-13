25c76768edb49d824ef28967220825db099be686
Daisuke Nishio <hey.alohaloha@gmail.com>
2026-09-13 14:10:12 +0900

    [B-853] separate Reference processor actions from Work transport setup

--- juce_shell/src/PluginProcessor.cpp
+++ juce_shell/src/PluginProcessor.cpp
@@ -1006,51 +1006,7 @@ void KirinHyphaProcessorBase::enableWritesNow()
     persistDawSessionUuid = juce::String::fromUTF8 (id.daw_session_uuid);
     persistName           = juce::String::fromUTF8 (id.name);
 
-#if KIRIN_HYPHA_GUIDE_TRANSPORT
-    if (preDisplayController == nullptr)
-        preDisplayController = std::make_unique<hypha::pre_display::Controller> (preDisplayClock);
-    if (role == Role::Post && captureWorkAttachmentController == nullptr)
-        captureWorkAttachmentController =
-            std::make_unique<hypha::capture::WorkAttachmentController>();
-   #if ! KIRIN_HYPHA_PRE_DISPLAY
-    if (role == Role::Post && referenceAuditionController == nullptr)
-        createReferenceAuditionController();
-   #endif
-    hypha::pre_display::RuntimeIdentity displayIdentity;
-    displayIdentity.role = role == Role::Post ? hypha::pre_display::GuideTargetRole::post
-                                              : hypha::pre_display::GuideTargetRole::pre;
-    displayIdentity.instanceId = persistInstanceId;
-    displayIdentity.projectUuid = persistProjectUuid;
-    displayIdentity.dawSessionUuid = persistDawSessionUuid;
-    displayIdentity.name = persistName;
-    displayIdentity.pluginVersion = JucePlugin_VersionString;
-    displayIdentity.pluginFormat = hypha::plugin_format::name (wrapperType);
-       #if JUCE_WINDOWS
-    displayIdentity.platform = "windows";
-       #else
-    displayIdentity.platform = "macos";
-       #endif
-       #if JUCE_ARM
-    displayIdentity.architecture = "arm64";
-       #else
-    displayIdentity.architecture = "x86_64";
-       #endif
-    preDisplayController->configureAndStart (std::move (displayIdentity));
-   #if ! KIRIN_HYPHA_PRE_DISPLAY
-    if (role == Role::Post && referenceAuditionController != nullptr)
-    {
-        const auto work = preDisplayController->connectedWorkReference();
-        if (work.valid())
-        {
-            hypha::reference_audition::RuntimeIdentity referenceIdentity;
-            referenceIdentity.runtimeInstanceId = work.runtimeInstanceId;
-            referenceIdentity.workId = work.workId;
-            referenceAuditionController->configure (
-                std::move (referenceIdentity), preparedSampleRate, preparedInputChannels);
-        }
-    }
-   #endif
-#endif
+    configureWorkTransports();
 
     writesEnabled.store (true, std::memory_order_release);
     analysisApplication.engineReady();
